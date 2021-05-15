8919b01f9c9b6febf870593035dde93241ce1d0f
Deyna Contreras <deyna.alexac@gmail.com>
2021-05-15 13:40:02 -0400

    try to run seed in heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,40 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ##Creating Users
-
+User.destroy_all
+10.times do
+       User.create(
+              name: Faker::Name.name,
+              rut: Faker::ChileRut.rut, 
+              email: Faker::Internet.email,
+              password: Faker::Internet.password(min_length: 10, max_length: 20)
+       )
+end
 
 ##Creating Properties
+Property.destroy_all
+array_property = []
+20.times do |prop|
+       prop = Property.create(
+              price: rand(250.000..900.000),
+              direction: Faker::Address.street_address,
+              area: rand(60..500).to_s + "mt2",
+              rooms: rand(1..5),
+              bathrooms: rand(1..5),
+              user_id: rand(1..10)
+       )
+       array_property.push(prop)
+end
 
+array_property.each do |property|
+       property.photo.attach(io: File.open('app/assets/images/prueba1.jpeg'), filename: "casa de prueba", content_type: "image/jpeg")
+end
 
 ##Creating Likes
+Like.destroy_all
+20.times do
+       Like.create(
+              user_id: rand(1..10),
+              property_id: rand(1..20)
+       )
+end
